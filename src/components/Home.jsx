c6853688abbeb1ab1cddb91a0260e773ed4c18a3
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-10-21 23:43:46 -0400

    add searchbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import Nav from "./Nav";
+import SearchBar from "./Searchbar";
 import instance from "../utils/axiosConfig";
 import { getUser } from "../utils/selectors";
 
@@ -21,6 +23,19 @@ const Home = () => {
             }); 
     }, []);
 
+    useEffect(() => {
+        axios.get('https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCbCvAB05gA9TWOT7FWCNpJvOTDAPufP_k')
+            .then(function (response) {
+                console.log('bookResp', response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            }); 
+    }, []);
+
     const [club, setClub] = useState("");
 
     const handleSubmit = (e) => {
@@ -32,6 +47,7 @@ const Home = () => {
     return (
         <>
             <Nav user={user || {}}/>
+            <SearchBar />
             <main className='container'>
                 <h2 className='homeTitle'>Create a Book Club</h2>
                 <form className='form' onSubmit={handleSubmit}>

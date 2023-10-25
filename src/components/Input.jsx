cf52fc9097bc968c8994aacabf83fecb3a045651
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-10-24 23:34:45 -0400

    custom Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+const Input = (props) => {
+  const { type, label, value, name, placeholder, error, disabled, onChange} = props;
+
+  return (
+    <div className="input-wrapper">
+      <label htmlFor={label}>{label}</label>
+      <input
+        type={type}
+        id={label}
+        value={value}
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {error && <p className="error">Input filed can't be empty!</p>}
+    </div>
+  )
+}
+
+Input.propTypes = {
+    type: 'text' | 'number' | 'email' | 'password',
+    label: PropTypes.string,
+    value: PropTypes.string | PropTypes.number,
+    name: PropTypes.string,
+    placeholder: PropTypes.string,
+    error: PropTypes.bool,
+    disabled: PropTypes.bool,
+    onChange: PropTypes.func
+}
+
+export default Input

18be98d41400aaac5b545c42003e6f3b0a42087c
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-10-26 23:55:09 -0400

    update search logic

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,16 +2,22 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const SearchBar = () => {
+  // const [searched, setSearched] = useState(false);
   const [searchInput, setSearchInput] = useState('');
+  const [searchTypeValue, setSearchTypeValue] = useState('');
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
 
   const results = {title: 'My Favourite Book', author: 'My Favourite Author'};
   console.log('title', title)
+  console.log('STV', searchTypeValue)
 
   useEffect(() => {
+    const searchType = document.getElementById("searchCategory") ?? 'title';
+    setSearchTypeValue(searchType.options[searchType.selectedIndex]);
+    console.log('*****', searchType);
     // axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCbCvAB05gA9TWOT7FWCNpJvOTDAPufP_k`)
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}+&key=AIzaSyCbCvAB05gA9TWOT7FWCNpJvOTDAPufP_k`)
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTypeValue}+&key=AIzaSyCbCvAB05gA9TWOT7FWCNpJvOTDAPufP_k`)
         .then(function (response) {
             console.log('bookResp', response);
         })
@@ -33,6 +39,7 @@ const handleSearch = (e) => {
   setTitle(title);
   console.log('titleSET', title);
   setAuthor(author);
+  // setSearched(true);
 }
 
   return (
@@ -45,7 +52,12 @@ const handleSearch = (e) => {
               type="text"
               placeholder="Search here"
               onChange={handleChange}
+              onSubmit={handleSearch}
               value={searchInput} />
+            <select id="searchCategory" name="searchCategory">
+                <option value="title">Title</option>
+                <option value="author">Author</option>
+            </select>
             <button className="searchButton" type="submit" onSubmit={handleSearch}>?</button>
           </div>  
         </form>  

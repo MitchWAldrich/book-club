c776cf1d90f6c594bdd87323383b647625a2a0f7
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-10-28 23:34:33 -0400

    move BookItem logic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import BookItem from "./BookItem";
 import Goal from "./Goal";
 import Nav from "./Nav";
 import SearchBar from "./Searchbar";
@@ -32,26 +31,10 @@ const Home = () => {
         setClub("");
     };
 
-    const sampleBook = {
-        author: 'Daniel Keyes',
-        categories: ['Fiction', 'Non-Fiction'],
-        averageRating: 4,
-        description: 'Mentally retarded Charlie Gordon participates in an experiment which turns him into a genius, but only temporarily.',
-        imageLinks: {
-          smallThumbnail: "http://books.google.com/books/content?id=NRWlitmahXkC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-          thumbnail: "http://books.google.com/books/content?id=NRWlitmahXkC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        },
-        language: 'en',
-        pageCount: 328,
-        publisher: 'Houghton Mifflin Harcourt',
-        title: 'Flowers for Algernon'
-      };
-
     return (
         <>
             <Nav user={user || {}}/>
             <Goal />
-            <BookItem author={sampleBook.author} categories={sampleBook.categories} averageRating={sampleBook.averageRating} description={sampleBook.description} imageLinks={sampleBook.imageLinks} language={sampleBook.language} pageCount={sampleBook.pageCount} publisher={sampleBook.publisher} title={sampleBook.title}/>
             <SearchBar />
             <main className='container'>
                 <h2 className='homeTitle'>Create a Book Club</h2>

55b2af8dd16f841d351a9be5807aa70e0ba02d5f
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-02 23:51:12 -0400

    format query response

--- src/utils/selectors.js
+++ src/utils/selectors.js
@@ -1,3 +1,19 @@
 export const getUser = (users, id) => {
-  return users.find(user => user.id === id)
+  return users.find((user) => user.id === id);
+};
+
+export const getBooks = (bookArray) => {
+  const bookObjects = bookArray.map((book) => ({
+    ["authors"]: book.volumeInfo.authors ?? null,
+    ["categories"]: book.volumeInfo.categories ?? null,
+    ["averageRating"]: book.volumeInfo.averageRating ?? null,
+    ["description"]: book.volumeInfo.description ?? null,
+    ["imageLinks"]: book.volumeInfo.imageLinks ?? null,
+    ["language"]: book.volumeInfo.language ?? null,
+    ["pageCount"]: book.volumeInfo.pageCount ?? null,
+    ["publisher"]: book.volumeInfo.publisher ?? null,
+    ["title"]: book.volumeInfo.title ?? null,
+  }));
+
+  return bookObjects;
 };

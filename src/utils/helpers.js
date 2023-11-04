ef46217b4369715ae7591ae73e8af13fcf6139ab
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-03 23:46:02 -0400

    moved functions into helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8,3 +8,15 @@ export const getFontSize = ( textLength, size ) => {
   return `${fontSize}vw`
 };
 //customize further
+
+export const shortenDescription = (descriptionString) => {
+  let blurb = descriptionString;
+
+  if (blurb.length >= 150) {
+    blurb = blurb.substring(0, 150);
+    const lastSpaceIndex = blurb.lastIndexOf(" ");
+    blurb = `${blurb.substring(0, lastSpaceIndex || 150)} ...`;
+  }
+
+  return blurb;
+};

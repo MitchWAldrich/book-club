7cbb399bfaf31c83d60ee72f6ccece76c2e5d864
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-09 22:04:23 -0500

    clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import React from "react";
-// import axios from "axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Library from "./components/Library";
@@ -9,22 +7,6 @@ import Replies from "./components/Replies";
 import './App.scss';
 
 function App() {
-  // Axios({
-  //   method: "GET",
-  //   url: "http://localhost:4000/",
-  //   headers: {
-  //     "Content-Type": "application/json"
-  //   }
-  // }).then(res => {
-  //   console.log(res.data.message);
-  // });
-  // const baseURL = "http://localhost:5173/";
-
-  // React.useEffect(() => {
-  //   axios.get(baseURL).then((response) => {
-  //     //
-  //   });
-  // }, []);
 
   return (
     <div>

0ce915b53fdfeb613226a298cb83d087666b72bc
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-13 23:36:22 -0500

    book club skeleton

---
+++ src/components/BookClub.jsx
@@ -0,0 +1,23 @@
+const BookClub = () => {
+
+  const bookClub = {
+    id: 1,
+    name: 'My First Book Club',
+    members: [ 'userId1', 'userId2', 'userId3', 'userId4', 'userId5'],
+    currentBook: 'Reese\'s Favourite Book',
+    nextBook: 'Oprah\'s Favourite Book',
+    previousBooks: ['Old Book', 'Other Old Book', 'A Graphic Novel'],
+    meetingFrequency: 'weekly',
+    nextMeetingDate: '12/24/2023'
+}
+
+  //invite members
+
+  //set book (and next/future book(s))
+
+  //archive book
+
+  //schedule meeting
+}
+
+export default BookClub;

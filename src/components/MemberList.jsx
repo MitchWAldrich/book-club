15ab165c70e13d35b0c1c5434d38a4e649ea6b4b
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-15 23:47:35 -0500

    add member button

--- src/components/MemberList.jsx
+++ src/components/MemberList.jsx
@@ -1,19 +1,39 @@
+import instance from './../utils/axiosConfig.js'
+
 import PropTypes from 'prop-types';
 
 import MemberListItem from "./MemberListItem";
 
 const MemberList = (props) => {
-  const { members } = props;
+  const { members, bookClubId } = props;
+
+  const invitee = 'userId1xxx';
+
+    //invite members
+    //2 statuses: invited, accepted
+    const inviteMember = (e) => {
+      () => e.preventdefault();
+      instance.patch(`/api/bookclubs/${bookClubId}`, { userId: invitee, bookClubId: bookClubId })
+     .then(response => {
+      console.log('invite response', response.data)  
+    })
+    .catch(error => console.error('invite error', error)); 
+    }
 
   return (
-    <main>
-      <MemberListItem members={members}/>
+    <main className="members-container">
+      <h3>Members</h3>
+      <div>
+        <MemberListItem members={members}/>
+      </div>
+      <button type="button" className="member-add-button" onClick={inviteMember}>Invite Member</button>
     </main>
   )
 }
 
 MemberList.propTypes = {
-  members: PropTypes.array
+  members: PropTypes.array,
+  bookClubId: PropTypes.string || PropTypes.number
 }
 
 export default MemberList;

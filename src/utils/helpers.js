5a3631a380e978796b4bb4f524c5a61779005dd8
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-17 01:57:46 -0500

    format obj by location function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20,3 +20,65 @@ export const shortenDescription = (descriptionString) => {
 
   return blurb;
 };
+
+export const formatByLocation = (location) => {
+  // for (let i = 0; i < locationsArray.length; i++) {
+  // let singleBookClassName = '';
+  // let singleBookTitleInfo = '';
+
+  const formattedObj = {
+    'className': '',
+    'titleInfo': '',
+    'titleText': '',
+    'authorInfo': '',
+    'authorText': '',
+    'smallHeadingText': '',
+    'titleAndAuthorInfo': '',
+    'information': '',
+    'left': '',
+    'image': ''
+  };
+
+  if (location === 'expanded') {
+    formattedObj['className'] = 'singleBookItem';
+    formattedObj['titleInfo'] = 'singleBookTitleInfo';
+    formattedObj['titleText'] = 'singleBookTitleText';
+    formattedObj['authorInfo'] = 'singleBookAuthorInfo';
+    formattedObj['authorText'] = 'singleBookAuthorText';
+    formattedObj['smallHeadingText'] = 'singleBookSmallHeadingText';
+    formattedObj['titleAndAuthorInfo'] = 'singleBookTitleAndAuthorInfo';
+    formattedObj['information'] = 'singleBookInformation';
+    formattedObj['left'] = 'singleBookItemLeft';
+    formattedObj['image'] = 'singleBookImage';
+  
+  } 
+  else if (location === 'listItem') {
+    formattedObj['className'] = 'singleBookItem';
+    formattedObj['titleInfo'] = 'singleBookTitleInfo';
+    formattedObj['titleText'] = 'singleBookTitleText';
+    formattedObj['authorInfo'] = 'singleBookAuthorInfo';
+    formattedObj['authorText'] = 'singleBookAuthorText';
+    formattedObj['smallHeadingText'] = 'singleBookSmallHeadingText';
+    formattedObj['titleAndAuthorInfo'] = 'singleBookTitleAndAuthorInfo';
+    formattedObj['information'] = 'singleBookInformation';
+    formattedObj['left'] = 'singleBookItemLeft';
+    formattedObj['image'] = 'singleBookImage';
+  
+  } 
+  else if (location === 'bookClubFeature') {
+    formattedObj['className'] = 'bookClubFeature';
+    formattedObj['titleInfo'] = 'bookClubFeatureTitleInfo';
+    formattedObj['titleText'] = 'bookClubFeatureTitleText';
+    formattedObj['authorInfo'] = 'bookClubFeatureAuthorInfo';
+    formattedObj['authorText'] = 'bookClubFeatureAuthorText';
+    formattedObj['smallHeadingText'] = 'bookClubFeatureSmallHeadingText';
+    formattedObj['titleAndAuthorInfo'] = 'bookClubFeatureTitleAndAuthorInfo';
+    formattedObj['information'] = 'bookClubFeatureInformation';
+    formattedObj['left'] = 'bookClubFeatureLeft';
+    formattedObj['image'] = 'bookClubFeatureImage';
+
+  }
+  // }
+
+  return formattedObj;
+}

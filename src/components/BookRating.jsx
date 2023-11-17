56225b8cbc1b56d8197daaad7fbe7b9551202ea4
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-16 20:49:14 -0500

    make dynamic

--- src/components/BookRating.jsx
+++ src/components/BookRating.jsx
@@ -4,23 +4,27 @@ import { IconContext } from 'react-icons';
 import { AiFillStar } from 'react-icons/ai';
 import { AiOutlineStar } from 'react-icons/ai';
 
-const BookRating = (rating) => {
+const BookRating = (props) => {
+  const { location, averageRating } = props;
   const stars = [1, 2, 3, 4, 5];
 
+  const starSize = location === 'expanded' ? '2em' : '1em';
+  const className = location === 'expanded' ? 'ratingStarsHor' : 'ratingStarsVert';
+
   return (
-    <div className="ratingStars">
-      {stars.map((star, key) =>
-        star <= rating ? (
+    <div className={className}>
+      {stars.reverse().map((star, key) =>
+        star <= averageRating ? (
           <IconContext.Provider
             key={key}
-            value={{ color: "gold", size: "1.3em", className: "review-star" }}
+            value={{ color: "gold", size: {starSize}, className: "review-star" }}
           >
             <AiFillStar />
           </IconContext.Provider>
         ) : (
           <IconContext.Provider
             key={key}
-            value={{ size: "1.3em", className: "review-star" }}
+            value={{ size: {starSize}, className: "review-star" }}
           >
             <AiOutlineStar />
           </IconContext.Provider>
@@ -31,7 +35,8 @@ const BookRating = (rating) => {
 };
 
 BookRating.propTypes = {
-  rating: PropTypes.string || PropTypes.number
+  location: PropTypes.string,
+  averageRating: PropTypes.string || PropTypes.number
 }
 
 export default BookRating;

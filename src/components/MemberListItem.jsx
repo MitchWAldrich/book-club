114978086be2928e0759856f769ee1492da4017c
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-21 23:38:52 -0500

    update render display

--- src/components/MemberListItem.jsx
+++ src/components/MemberListItem.jsx
@@ -5,12 +5,32 @@ import MemberItem from './MemberItem';
 const MemberListItem = (props) => {
   const { members } = props;
 
+  const renderMembers = (membersArray) => {
+    
+    if (!membersArray.length && membersArray) {
+      <MemberItem image={membersArray.image} userName={membersArray.username} isOnline={membersArray.isOnline}/>
+    } else null
+
+    if (membersArray.length === 1) {
+      return (
+        <MemberItem image={membersArray[0].image} userName={membersArray[0].username} isOnline={membersArray[0].isOnline}/>
+      )
+    }
+
+    if (membersArray.length > 1) {
+      return (
+        members.map( (member, key) => (
+          <MemberItem key={key} image={member.image} userName={member.username} isOnline={member.isOnline}/>
+        ))
+      )
+    }
+  }
+
+  const memberDisplay = renderMembers(members);
+
   return (
     <div className="member-list">
-      {members.map( (member, key) => (
-        <MemberItem key={key} image={member.image} userName={member.userName} isOnline={member.isOnline}/>
-      ))
-      }
+      {memberDisplay}
     </div>
   )
 }

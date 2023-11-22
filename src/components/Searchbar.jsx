5db9efed4b1a71d04e883d572c98370ee69e0c0d
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-11-21 23:40:10 -0500

    styling and logic updates to searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,20 +1,34 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import instance from "../utils/axiosConfig";
+import PropTypes from 'prop-types';
+
+import { IconContext } from 'react-icons';
+import { HiOutlineSearchCircle } from "react-icons/hi";
+
 import BookListItem from "./BookListItem";
 import Dropdown from "./Dropdown";
 import Input from "./Input";
-import { getBooks } from "../utils/selectors";
 
-const SearchBar = () => {
-  const [searched, setSearched] = useState(false);
-  const [error, setError] = useState(false);
-  const [searchInput, setSearchInput] = useState("");
-  const [searchTypeValue, setSearchTypeValue] = useState("title");
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [bookResponse, setBookResponse] = useState([]);
+import { getBooks, getUserByUserId } from "../utils/selectors";
+import MemberList from "./MemberList";
 
-  // const results = {title: 'My Favourite Book', author: 'My Favourite Author'};
+import { usersMock } from "../mocks/users";
+
+const SearchBar = (props) => {
+  const { className, location, dropDown, id, valueCallback } = props;
+
+  const [ bookSearched, setBookSearched ] = useState(false);
+  const [ userSearched, setUserSearched ] = useState(location === 'bookClub' ? true : false);
+  const [ error, setError ] = useState(false);
+  const [ searchInput, setSearchInput ] = useState("");
+  const [ searchTypeValue, setSearchTypeValue ] = useState("title");
+  const [ title, setTitle ] = useState("");
+  const [ author, setAuthor ] = useState("");
+  const [ bookResponse, setBookResponse ] = useState([]);
+  const [ userResponse, setUserResponse ] = useState([]);
+  // const [ searchResult, setSearchResult ] = useState(null);
+  const [ bookId, setBookId ] = useState(null);
 
   const units = ["Title", "Author"];
 
@@ -22,6 +36,11 @@ const SearchBar = () => {
     setSearchTypeValue(dropdownValue);
   };
 
+  const getClicked = (clickedValue) => {
+    setBookId(clickedValue)
+  };
+console.log('clickedBookId', bookId)
+// console.log('bookSearched', bookSearched);
   useEffect(() => {
     axios
       .get(
@@ -38,7 +57,25 @@ const SearchBar = () => {
       .finally(function () {
         // always executed
       });
-  }, [searched]);
+  // }, [bookSearched, searchTypeValue, searchInput]);
+}, [bookSearched]);
+  
+    useEffect(() => {
+    instance
+      .get(`/api/users/${id}`)
+      .then(function (response) {
+        console.log("userResp", response);
+        const friendsObjs = response.data.user.friends.accepted.map((friend) => getUserByUserId(usersMock, friend))
+        setUserResponse(friendsObjs);
+        //Set up Book Item response
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, [userSearched]);
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -52,49 +89,61 @@ const SearchBar = () => {
     } else {
       setError(false);
     }
-    setTitle(title);
-    setAuthor(author);
-    setSearched(true);
+    console.log('**SEARCHED**')
+  
+    if ( location === 'home' || location === 'bookClubFirstBook') {
+      setTitle(title);
+      setAuthor(author);
+      setBookSearched(true);
+    }
+
+    if ( location === 'bookClub' ) setUserSearched(true);
+    if ( location === 'bookClubFirstBook') {
+      () => valueCallback(bookId)
+      console.log('bookIdCallback', bookId);
+    }
   };
 
   // add Loading spinner
   return (
     <>
-      <div className="container">
+      <div>
         <form onSubmit={handleSearch}>
-          <div className="searchBar">
+          <div className={"searchBar"}>
             <Input
-              className="searchInput"
+              className={className}
               type="text"
-              label="search"
+              // label="search"
               value={searchInput}
               name="searchInput"
               error={error}
               onChange={handleChange}
               placeholder="Search here"
             />
-            <Dropdown
+            {dropDown ? (
+              <Dropdown
               category={"Search Type"}
               options={units}
               valueCallback={getUnit}
             />
-            <button
-              className="searchButton"
-              type="text"
-              onSubmit={() => handleSearch()}
+            ) : null }
+            <IconContext.Provider
+            // key={key}
+            value={{ color: 'teal', size: "100%", className: "searchButton" }}
             >
-              ?
-            </button>
+              <HiOutlineSearchCircle onClick={handleSearch} />
+            </IconContext.Provider>
           </div>
         </form>
       </div>
       <br></br>
 
       <br></br>
-      {searched
+      {bookSearched
         ? bookResponse.map((book, key) => (
             <BookListItem
               key={key}
+              bookId={book.bookId}
               authors={book.authors}
               categories={book.categories}
               averageRating={book.averageRating}
@@ -104,11 +153,21 @@ const SearchBar = () => {
               pageCount={book.pageCount}
               publisher={book.publisher}
               title={book.title}
+              valueCallback={getClicked}
             />
           ))
         : null}
+      {userSearched ? <MemberList members={userResponse} /> : null}
     </>
   );
 };
+
+SearchBar.propTypes = {
+  className: PropTypes.string,
+  location: PropTypes.string,
+  dropDown: PropTypes.bool,
+  id: PropTypes.string,
+  valueCallback: PropTypes.func,
+}
 
 export default SearchBar;

bd26f0f22bfd3818778a110694bb26200b5bbaa7
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-12-01 22:58:49 -0500

    create book clubs list component

---
+++ src/components/BookClubsList.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+
+import BookClubListItem from "./BookClubListItem";
+
+const BookClubsList = (props) => {
+  return (
+    <BookClubListItem
+    // key={key}
+    // bookClubObj={bookClub}
+    // userId={user.userId}
+    />
+  );
+};
+
+BookClubsList.propTypes = {};
+
+export default BookClubsList;

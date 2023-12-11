309a56bc7bffc42ed854b9ba51ec2bbe9d255d9c
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-12-10 23:58:20 -0500

    add more mocks

--- src/mocks/bookClubs.js
+++ src/mocks/bookClubs.js
@@ -90,7 +90,99 @@ export const bookClubsMock = [
       isNewBookClub: false,
       visibility: 'inviteOnly',
       categories: ['Non-Fiction'],
-  }
+  },
+  {
+    id: 4,
+    bookClubId: 'dvu89w(l3n',
+    bookClubHostId: '62jt*(kj!3',
+    hostUserName: 'Snuggz',
+    name: 'Fantasy Fans',
+    members: {invited: [], accepted: [], requested: []},
+    books: {
+      currentBook: {title: 'Birds', author: 'Smart Guy', thumbnail: ''},
+      nextBook: 'Yakkity Yak',
+      upcomingBooks: ['I Love Books', 'My Best Day', 'Nobody Knows'],
+      previousBooks: ['Read It All', 'Can I Borrow A Feeling?', 'Batman'],
+    },
+    meetings: {
+      meetingFrequency: 'monthly',
+      nextMeetingDate: '01/12/2024',
+      nextMeetingTime: '7:30pm',
+      nextMeetingLocation: {
+        online: 'N/A',
+        inPerson: {
+          'streetNumber': 765,
+          'unitNumber': '',
+          'streetName': 'Brock Ave',
+          'city': 'London',
+          'province': 'ON',
+          'country': 'CA'
+        }
+      }
+    },
+    isNewBookClub: false,
+    visibility: 'friendsCanSee',
+    categories: ['Fantasy'],
+},
+{
+    id: 5,
+    bookClubId: '4gfu6)u$ep',
+    bookClubHostId: '523dgf*5gn&',
+    hostUserName: 'Jack Antonoff',
+    name: 'Pop Jams',
+    members: {invited: [],  accepted: ['62jt*(kj!3', '34xc98(dfk'], requested: []},
+    books: {
+      currentBook: {title: 'Book: Taylor\'s Version', author: 'Taylor Swift', thumbnail: ''},
+      nextBook: 'Buy An Island',
+      upcomingBooks: ['45 Bloor Street', 'Cats', 'Katy Perry: A Biography'],
+      previousBooks: ['When I Was Young', 'Goosebumps', 'Cher'],
+    },
+    meetings: {
+      meetingFrequency: 'weekly',
+      nextMeetingDate: '01/04/2024',
+      nextMeetingTime: '7:00pm',
+      nextMeetingLocation: {
+        online: 'Zoom.secondlink',
+        inPerson: 'N/A'
+      }
+    },
+    isNewBookClub: false,
+    visibility: 'public',
+    categories: ['Anything'],
+},
+{
+    id: 6,
+    bookClubId: 'fdul694*',
+    bookClubHostId: '34xc98(dfk',
+    hostUserName: 'Jamie Smith',
+    name: 'Book Clubbing',
+    members: {invited: ['62jt*(kj!3'], accepted: ['523dgf*5gn&'], requested: []},
+    books: {
+      currentBook: {title: 'Reese\'s 3rd Favourite Book', author: 'Luna Slink', thumbnail: ''},
+      nextBook: 'Oprah\'s 3rd Favourite Book',
+      upcomingBooks: ['Three and One Book', 'Books are Fun', 'Anatomy of a Society'],
+      previousBooks: ['Old Book 3', 'Third Old Book', 'A 3rd Graphic Novel'],
+    },
+    meetings: {
+      meetingFrequency: 'bi-weekly',
+      nextMeetingDate: '11/24/2023',
+      nextMeetingTime: '7:00pm',
+      nextMeetingLocation: {
+        online: 'Zoom.otherLink',
+        inPerson: {
+          'streetNumber': 12,
+          'unitNumber': 'N/A',
+          'streetName': 'Dundas St. W',
+          'city': 'Toronto',
+          'province': 'ON',
+          'country': 'CA'
+        }
+      } 
+    },
+    isNewBookClub: false,
+    visibility: 'inviteOnly',
+    categories: ['Non-Fiction'],
+}
 ]
 
 export const bookClubMock = bookClubsMock[0];
--- src/mocks/users.js
+++ src/mocks/users.js
@@ -118,7 +118,127 @@ export const usersMock = [
         requested: [],
         accepted: [ '34xc98(dfk', '523dgf*5gn&' ]
       }
-  }
+  },
+  {
+    id: 4,
+    userId: '34xc98(dfk',
+    email: 'user1@email.com',
+    password: 'password',
+    username: 'Jamie Smith',
+    image: 'https://www.perfocal.com/blog/content/images/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg',
+    goals: [],
+    library: {
+        haveRead: [],
+        toRead: []
+    },
+    bookClubs: {
+        host: ['fdul694*'],
+        invited: [],
+        accepted: ['sdfjil234)', 'fsqjil$t72'],
+        requested: [],
+    },
+    friends: {
+      requested: [],
+      accepted: [ '523dgf*5gn&', '62jt*(kj!3' ]
+    }
+},
+{
+    id: 5,
+    userId: '523dgf*5gn&',
+    email: 'user2@email.com',
+    password: 'password',
+    username: 'Jenna Black',
+    image: 'https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture.jpg',
+    goals: [],
+    library: {
+        haveRead: [],
+        toRead: []
+    },
+    bookClubs: {
+        host: ['fsqjil$t72'],
+        invited: ['sdfjil234)'],
+        accepted: ['fdul694*'],
+        requested: []
+    },
+    friends: {
+      requested: [],
+      accepted: [ '34xc98(dfk', '62jt*(kj!3' ]
+    }
+},
+{
+    id: 6,
+    userId: '62jt*(kj!3',
+    email: 'user3@email.com',
+    password: 'password',
+    username: 'Amari Bolt',
+    image: 'https://www.elitesingles.co.uk/wp-content/uploads/sites/59/2019/11/2b_en_articleslide_sm2-350x264.jpg',
+    goals: [{
+        name: 'My First Goal',
+        number: '20 pages',
+        timeline: '3 days',
+    }],
+    library: {
+        haveRead: [{
+            title: 'title1',
+            authors: [ 'John Smith', 'Julie Black' ],
+            categories: [ 'Non-Fiction' ],
+            averageRating: 5,
+            description: 'A great book about things',
+            imageLinks: { smallThumbnail: 'http:', thumbnail: 'https:' },
+            pageCount: 234,
+            publisher: 'Clearly'
+          },{
+            title: 'title2',
+            authors: [ 'Fred Smith', 'Julie Steinberg' ],
+            categories: [ 'Fiction' ],
+            averageRating: 4,
+            description: 'A great book about stuff',
+            imageLinks: { smallThumbnail: 'http:', thumbnail: 'https:' },
+            pageCount: 154,
+            publisher: 'Smithson'
+          },
+          {
+            title: 'title3',
+            authors: [ 'Fred Smith', 'Julie Steinberg' ],
+            categories: [ 'Fiction' ],
+            averageRating: 3,
+            description: 'A great novel about stuff',
+            imageLinks: { smallThumbnail: 'http:', thumbnail: 'https:' },
+            pageCount: 200,
+            publisher: 'Smithly'
+          }],
+          toRead: [{
+            title: 'title4',
+            authors: [ 'Whit Merrifield'],
+            categories: [ 'Biography', 'Sports' ],
+            averageRating: 5,
+            description: 'A great book about Whit',
+            imageLinks: { smallThumbnail: 'http:', thumbnail: 'https:' },
+            pageCount: 234,
+            publisher: 'Jays'
+          },
+          {
+            title: 'title5',
+            authors: [ 'Fred Smith Black', 'Julie Steinberg' ],
+            categories: [ 'Fiction' ],
+            averageRating: 4,
+            description: 'A great book about stuff',
+            imageLinks: { smallThumbnail: 'http:', thumbnail: 'https:' },
+            pageCount: 34,
+            publisher: 'New'
+          }]
+    },
+    bookClubs: {
+        host: ['sdfjil234)'],
+        invited: ['fdul694*'],
+        accepted: ['fsqjil$t72'],
+        requested: []
+    },
+    friends: {
+      requested: [],
+      accepted: [ '34xc98(dfk', '523dgf*5gn&' ]
+    }
+}
 ];
 
 export const userMock = usersMock[2];

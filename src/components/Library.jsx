1f23a8bd3f538e1b1346e46939293ca545ac0f9e
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-12-23 15:33:03 -0500

    update book props

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -18,47 +18,50 @@ const Library = () => {
         {user?.library?.toRead.map((book, key) => (
           <BookListItem
             key={key}
-            bookId={book.bookId}
-            authors={book.authors}
-            categories={book.categories}
-            averageRating={book.averageRating}
-            description={book.description}
-            imageLinks={book.imageLinks}
-            language={book.language}
-            pageCount={book.pageCount}
-            publisher={book.publisher}
-            title={book.title}
+            bookObj={book}
+            // bookId={book.bookId}
+            // authors={book.authors}
+            // categories={book.categories}
+            // averageRating={book.averageRating}
+            // description={book.description}
+            // imageLinks={book.imageLinks}
+            // language={book.language}
+            // pageCount={book.pageCount}
+            // publisher={book.publisher}
+            // title={book.title}
           />
         )) ?? null}
         <h2>Books I've Read</h2>
         {user?.library?.haveRead.map((book, key) => (
           <BookListItem
             key={key}
-            bookId={book.bookId}
-            authors={book.authors}
-            categories={book.categories}
-            averageRating={book.averageRating}
-            description={book.description}
-            imageLinks={book.imageLinks}
-            language={book.language}
-            pageCount={book.pageCount}
-            publisher={book.publisher}
-            title={book.title}
+            bookObj={book}
+            // bookId={book.bookId}
+            // authors={book.authors}
+            // categories={book.categories}
+            // averageRating={book.averageRating}
+            // description={book.description}
+            // imageLinks={book.imageLinks}
+            // language={book.language}
+            // pageCount={book.pageCount}
+            // publisher={book.publisher}
+            // title={book.title}
           />
         )) ?? null}
         <br></br>
         <SingleBookItem
           // key={key}
-          bookId={bookMock.bookId}
-          authors={bookMock.authors}
-          categories={bookMock.categories}
-          averageRating={bookMock.averageRating}
-          description={bookMock.description}
-          imageLinks={bookMock.imageLinks}
-          language={bookMock.language}
-          pageCount={bookMock.pageCount}
-          publisher={bookMock.publisher}
-          title={bookMock.title}
+          bookObj={bookMock}
+          // bookId={bookMock.bookId}
+          // authors={bookMock.authors}
+          // categories={bookMock.categories}
+          // averageRating={bookMock.averageRating}
+          // description={bookMock.description}
+          // imageLinks={bookMock.imageLinks}
+          // language={bookMock.language}
+          // pageCount={bookMock.pageCount}
+          // publisher={bookMock.publisher}
+          // title={bookMock.title}
           userId={3}
           location={"expanded"}
         />

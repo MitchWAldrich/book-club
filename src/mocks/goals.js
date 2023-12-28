5c0eecf71d4305195aa105ee881485b78b6829be
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-12-27 22:37:37 -0500

    goal object mocks

---
+++ src/mocks/goals.js
@@ -0,0 +1,38 @@
+export const goalMock = {
+  id: 1,
+  goalId: 'dfS8*ur(o2',
+  goalName: 'My First Reading Goal',
+  goalUserId: '34xc98(dfk',
+  goal: 3,
+  goalUnit: 'books',
+  goalTimeline: 1,
+  goalTimelineUnit: 'month',
+  goalRecurrence: 'N/A',
+  goalRecurrenceUnit: 'N/A',
+}
+
+export const goalMock2 = {
+  id: 2,
+  goalId: 'rg^5hUi9lj',
+  goalName: 'My Second Reading Goal',
+  goalUserId: '523dgf*5gn&',
+  goal: 1,
+  goalUnit: 'book',
+  goalTimeline: 1,
+  goalTimelineUnit: 'month',
+  goalRecurrence: 1,
+  goalRecurrenceUnit: 'year',
+}
+
+export const goalMock3 = {
+  id: 3,
+  goalId: 'hjKp9&6ygw',
+  goalName: 'I Will Become a Reader',
+  goalUserId: '523dgf*5gn&',
+  goal: 1,
+  goalUnit: 'book',
+  goalTimeline: 1,
+  goalTimelineUnit: 'week',
+  goalRecurrence: 1,
+  goalRecurrenceUnit: 'year',
+}

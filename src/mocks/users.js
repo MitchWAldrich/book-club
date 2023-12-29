d56b808550dcbf1bf932ee9a2821d241755a39ea
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-12-29 14:35:41 -0500

    add book objs

--- src/mocks/users.js
+++ src/mocks/users.js
@@ -31,8 +31,216 @@ export const usersMock = [
       image: 'https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture.jpg',
       goals: ['rg^5hUi9lj', 'hjKp9&6ygw'],
       library: {
-          haveRead: [],
-          toRead: []
+          haveRead: [{
+            "title": "Google Hacks",
+            "authors": [
+                "Tara Calishain",
+                "Rael Dornfest"
+            ],
+            "publisher": "\"O'Reilly Media, Inc.\"",
+            "publishedDate": "2003",
+            "description": "Explains how to take advantage of Google's user interface, discussing how to filter results, use Google's special services, integrate Google applications into a Web site or Weblog, write information retrieval programs, and play games.",
+            "industryIdentifiers": [
+                {
+                    "type": "ISBN_10",
+                    "identifier": "0596004478"
+                },
+                {
+                    "type": "ISBN_13",
+                    "identifier": "9780596004477"
+                }
+            ],
+            "readingModes": {
+                "text": false,
+                "image": true
+            },
+            "pageCount": 356,
+            "printType": "BOOK",
+            "categories": [
+                "Computer software"
+            ],
+            "averageRating": 5,
+            "ratingsCount": 2,
+            "maturityRating": "NOT_MATURE",
+            "allowAnonLogging": false,
+            "contentVersion": "2.2.4.0.preview.1",
+            "panelizationSummary": {
+                "containsEpubBubbles": false,
+                "containsImageBubbles": false
+            },
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=GqTwl7-WZtUC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=GqTwl7-WZtUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+            "previewLink": "http://books.google.ca/books?id=GqTwl7-WZtUC&pg=PA52&dq=intitle:&hl=&cd=4&source=gbs_api",
+            "infoLink": "http://books.google.ca/books?id=GqTwl7-WZtUC&dq=intitle:&hl=&source=gbs_api",
+            "canonicalVolumeLink": "https://books.google.com/books/about/Google_Hacks.html?hl=&id=GqTwl7-WZtUC"
+          },
+          {
+            "title": "The Everything Store",
+            "subtitle": "Jeff Bezos and the Age of Amazon",
+            "authors": [
+                "Brad Stone"
+            ],
+            "publisher": "Little, Brown",
+            "publishedDate": "2013-10-15",
+            "description": "The authoritative account of the rise of Amazon and its intensely driven founder, Jeff Bezos, praised by the Seattle Times as \"the definitive account of how a tech icon came to life.\" Amazon.com started off delivering books through the mail. But its visionary founder, Jeff Bezos, wasn't content with being a bookseller. He wanted Amazon to become the everything store, offering limitless selection and seductive convenience at disruptively low prices. To do so, he developed a corporate culture of relentless ambition and secrecy that's never been cracked. Until now. Brad Stone enjoyed unprecedented access to current and former Amazon employees and Bezos family members, giving readers the first in-depth, fly-on-the-wall account of life at Amazon. Compared to tech's other elite innovators -- Jobs, Gates, Zuckerberg -- Bezos is a private man. But he stands out for his restless pursuit of new markets, leading Amazon into risky new ventures like the Kindle and cloud computing, and transforming retail in the same way Henry Ford revolutionized manufacturing. The Everything Store is the revealing, definitive biography of the company that placed one of the first and largest bets on the Internet and forever changed the way we shop and read.",
+            "industryIdentifiers": [
+                {
+                    "type": "ISBN_13",
+                    "identifier": "9780316219259"
+                },
+                {
+                    "type": "ISBN_10",
+                    "identifier": "0316219258"
+                }
+            ],
+            "readingModes": {
+                "text": true,
+                "image": false
+            },
+            "pageCount": 387,
+            "printType": "BOOK",
+            "categories": [
+                "Business & Economics"
+            ],
+            "maturityRating": "NOT_MATURE",
+            "allowAnonLogging": true,
+            "contentVersion": "1.23.24.0.preview.2",
+            "panelizationSummary": {
+                "containsEpubBubbles": false,
+                "containsImageBubbles": false
+            },
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=t95iRvivDHIC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=t95iRvivDHIC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+            "previewLink": "http://books.google.ca/books?id=t95iRvivDHIC&printsec=frontcover&dq=inAuthor:Ted+Bell&hl=&cd=8&source=gbs_api",
+            "infoLink": "https://play.google.com/store/books/details?id=t95iRvivDHIC&source=gbs_api",
+            "canonicalVolumeLink": "https://play.google.com/store/books/details?id=t95iRvivDHIC"
+          }],
+          toRead: [{
+            "title": "The Wildest Ride",
+            "subtitle": "A Novel",
+            "authors": [
+                "Marcella Bell"
+            ],
+            "publisher": "HQN Books",
+            "publishedDate": "2021-08-10",
+            "description": "“Filled with deep emotion and intense spark, Marcella Bell brings grit, spark and brilliance to western romance! Marcella Bell is one to watch!”—Maisey Yates, New York Times bestselling author The world watches on as reality TV meets rodeo in this competition like no other. In front of the cameras, Lil and AJ are each other’s biggest rivals. Off-screen, it’s about to get a whole lot more complicated… At thirty-six, undefeated rodeo champion AJ Garza is supposed to be retiring, not chasing after an all-new closed-circuit rodeo tour with a million-dollar prize. But with the Houston rodeo program that saved him as a wayward teen on the brink of bankruptcy, he’ll compete. And he’ll win. Enter Lilian Sorrow Island. Raised by her grandparents on the family ranch in Muskogee, Oklahoma, Lil is more a cowboy than city boy AJ will ever be. It shows. She’s not about to let him steal the prize that’ll save her ranch, even if he is breathtakingly magnificent, in pretty much every way going… This summer, in this bold, uplifting novel, Marcella Bell reminds us that even when it comes to rodeo, romance is the wildest ride of all! A Closed Circuit Novel",
+            "industryIdentifiers": [
+                {
+                    "type": "ISBN_13",
+                    "identifier": "9780369703606"
+                },
+                {
+                    "type": "ISBN_10",
+                    "identifier": "036970360X"
+                }
+            ],
+            "readingModes": {
+                "text": true,
+                "image": false
+            },
+            "pageCount": 414,
+            "printType": "BOOK",
+            "categories": [
+                "Fiction"
+            ],
+            "maturityRating": "NOT_MATURE",
+            "allowAnonLogging": true,
+            "contentVersion": "1.5.5.0.preview.2",
+            "panelizationSummary": {
+                "containsEpubBubbles": false,
+                "containsImageBubbles": false
+            },
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=_Z8AEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=_Z8AEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+            "previewLink": "http://books.google.ca/books?id=_Z8AEAAAQBAJ&printsec=frontcover&dq=inAuthor:Ted+Bell&hl=&cd=1&source=gbs_api",
+            "infoLink": "https://play.google.com/store/books/details?id=_Z8AEAAAQBAJ&source=gbs_api",
+            "canonicalVolumeLink": "https://play.google.com/store/books/details?id=_Z8AEAAAQBAJ"
+          },
+          {
+            "title": "Sourcing and Recruitment Handbook",
+            "authors": [
+                "Jay Tarimala"
+            ],
+            "publisher": "Jay Tarimala",
+            "publishedDate": "2020-11-10",
+            "description": "The sourcing and recruitment community has come a long way from what it used to be 20 years back and as recently as 5 years back and the amount of technology changes has kept everyone on the tenterhooks. The methods that you would adopt or Boolean strings that used to work today may not work tomorrow and there is a constant learning curve to keep oneself updated and also brings a healthy level of competition among individuals eager to show their wares. The volume based or niche skills hiring has become a race to the finish. The individuals with the super sourcing as well as relationship management skills tend to be more often than not the winners in the end. The recruitment community looks to find the needle in the haystack every day to find the right candidate to fulfill the hiring manager mandate. The ever bulging stock of search strings, productivity tricks and their ways of thinking they implement are a stuff of legend. The tools do not make a Sourcer or recruiter but they should always be seen as an assistant to free up their time for more productive conversations with prospective candidates. This exercise is an attempt to equip the Talent Acquisition community with the various ways and methods to optimize their time and find the relevant information (trends, movers and shakers etc.) to be better prepared to find relevant candidates for their clients (internal or external). It is by no means an exhaustive list but a start nonetheless.",
+            "readingModes": {
+                "text": false,
+                "image": true
+            },
+            "pageCount": 400,
+            "printType": "BOOK",
+            "categories": [
+                "Business & Economics"
+            ],
+            "maturityRating": "NOT_MATURE",
+            "allowAnonLogging": false,
+            "contentVersion": "0.1.1.0.preview.1",
+            "panelizationSummary": {
+                "containsEpubBubbles": false,
+                "containsImageBubbles": false
+            },
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=6OYHEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=6OYHEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+            },
+            "language": "en",
+            "previewLink": "http://books.google.ca/books?id=6OYHEAAAQBAJ&pg=PA118&dq=intitle:&hl=&cd=1&source=gbs_api",
+            "infoLink": "https://play.google.com/store/books/details?id=6OYHEAAAQBAJ&source=gbs_api",
+            "canonicalVolumeLink": "https://play.google.com/store/books/details?id=6OYHEAAAQBAJ"
+          },
+          {
+            "title": "Escape Clause",
+            "authors": [
+                "John Sandford"
+            ],
+            "publisher": "Penguin",
+            "publishedDate": "2016",
+            "description": "No, but I do.",
+            "industryIdentifiers": [
+                {
+                    "type": "ISBN_13",
+                    "identifier": "9780399168918"
+                },
+                {
+                    "type": "ISBN_10",
+                    "identifier": "0399168915"
+                }
+            ],
+            "readingModes": {
+                "text": false,
+                "image": false
+            },
+            "pageCount": 402,
+            "printType": "BOOK",
+            "categories": [
+                "Fiction"
+            ],
+            "maturityRating": "NOT_MATURE",
+            "allowAnonLogging": false,
+            "contentVersion": "0.1.0.0.preview.0",
+            "panelizationSummary": {
+                "containsEpubBubbles": false,
+                "containsImageBubbles": false
+            },
+            "imageLinks": {
+                "smallThumbnail": "http://books.google.com/books/content?id=K1wYDQAAQBAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                "thumbnail": "http://books.google.com/books/content?id=K1wYDQAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+            },
+            "language": "en",
+            "previewLink": "http://books.google.ca/books?id=K1wYDQAAQBAJ&dq=inAuthor:Ted+Bell&hl=&cd=5&source=gbs_api",
+            "infoLink": "http://books.google.ca/books?id=K1wYDQAAQBAJ&dq=inAuthor:Ted+Bell&hl=&source=gbs_api",
+            "canonicalVolumeLink": "https://books.google.com/books/about/Escape_Clause.html?hl=&id=K1wYDQAAQBAJ"
+          },]
       },
       bookClubs: {
           host: ['fsqjil$t72'],

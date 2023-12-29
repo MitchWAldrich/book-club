ae0c8ad030ba2fde8040e04282a0130edb2087c0
MitchWAldrich <mitch.w.aldrich@gmail.com>
2023-12-28 23:48:17 -0500

    goal mocks

--- src/mocks/goals.js
+++ src/mocks/goals.js
@@ -1,38 +1,44 @@
-export const goalMock = {
-  id: 1,
-  goalId: 'dfS8*ur(o2',
-  goalName: 'My First Reading Goal',
-  goalUserId: '34xc98(dfk',
-  goal: 3,
-  goalUnit: 'books',
-  goalTimeline: 1,
-  goalTimelineUnit: 'month',
-  goalRecurrence: 'N/A',
-  goalRecurrenceUnit: 'N/A',
-}
+export const goalsMock = [
+  {
+    id: 1,
+    goalId: 'dfS8*ur(o2',
+    goalName: 'My First Reading Goal',
+    goalUserId: '34xc98(dfk',
+    goal: 3,
+    goalUnit: 'books',
+    goalTimeline: 1,
+    goalTimelineUnit: 'month',
+    goalRecurrence: null,
+    goalRecurrenceUnit: null,
+  },
+  {
+    id: 2,
+    goalId: 'rg^5hUi9lj',
+    goalName: 'My Second Reading Goal',
+    goalUserId: '523dgf*5gn&',
+    goal: 1,
+    goalUnit: 'book',
+    goalTimeline: 1,
+    goalTimelineUnit: 'month',
+    goalRecurrence: 1,
+    goalRecurrenceUnit: 'year',
+  },
+  {
+    id: 3,
+    goalId: 'hjKp9&6ygw',
+    goalName: 'I Will Become a Reader',
+    goalUserId: '523dgf*5gn&',
+    goal: 1,
+    goalUnit: 'book',
+    goalTimeline: 1,
+    goalTimelineUnit: 'week',
+    goalRecurrence: 1,
+    goalRecurrenceUnit: 'year',
+  }
+]
 
-export const goalMock2 = {
-  id: 2,
-  goalId: 'rg^5hUi9lj',
-  goalName: 'My Second Reading Goal',
-  goalUserId: '523dgf*5gn&',
-  goal: 1,
-  goalUnit: 'book',
-  goalTimeline: 1,
-  goalTimelineUnit: 'month',
-  goalRecurrence: 1,
-  goalRecurrenceUnit: 'year',
-}
+export const goalMock = goalsMock[0];
 
-export const goalMock3 = {
-  id: 3,
-  goalId: 'hjKp9&6ygw',
-  goalName: 'I Will Become a Reader',
-  goalUserId: '523dgf*5gn&',
-  goal: 1,
-  goalUnit: 'book',
-  goalTimeline: 1,
-  goalTimelineUnit: 'week',
-  goalRecurrence: 1,
-  goalRecurrenceUnit: 'year',
-}
+export const goalMock2 = goalsMock[1];
+
+export const goalMock3 = goalsMock[2];

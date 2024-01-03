1e87b2f8d56a827e2a5350a817a4ab506a4ce5f3
MitchWAldrich <mitch.w.aldrich@gmail.com>
2024-01-02 23:46:21 -0500

    reformat as component, not page

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,73 +1,35 @@
-import { useContext } from "react";
+import PropTypes from "prop-types";
 
 import BookListItem from "./BookListItem";
-import Nav from "./Nav";
 import SingleBookItem from "./SingleBookItem";
 
-import userContext from "./../userContext";
-import { bookMock } from "../mocks/books";
-
-const Library = () => {
-  const user = useContext(userContext);
+const Library = (props) => {
+  const { userObj } = props;
 
   return (
     <main>
-      <Nav user={user ?? "Guest"} />
       <div>
         <h2>To Read List</h2>
-        {user?.library?.toRead.map((book, key) => (
-          <BookListItem
-            key={key}
-            bookObj={book}
-            // bookId={book.bookId}
-            // authors={book.authors}
-            // categories={book.categories}
-            // averageRating={book.averageRating}
-            // description={book.description}
-            // imageLinks={book.imageLinks}
-            // language={book.language}
-            // pageCount={book.pageCount}
-            // publisher={book.publisher}
-            // title={book.title}
-          />
+        {userObj?.library?.toRead.map((book, key) => (
+          <BookListItem key={key} bookObj={book} />
         )) ?? null}
         <h2>Books I've Read</h2>
-        {user?.library?.haveRead.map((book, key) => (
-          <BookListItem
-            key={key}
-            bookObj={book}
-            // bookId={book.bookId}
-            // authors={book.authors}
-            // categories={book.categories}
-            // averageRating={book.averageRating}
-            // description={book.description}
-            // imageLinks={book.imageLinks}
-            // language={book.language}
-            // pageCount={book.pageCount}
-            // publisher={book.publisher}
-            // title={book.title}
-          />
+        {userObj?.library?.haveRead.map((book, key) => (
+          <BookListItem key={key} bookObj={book} />
         )) ?? null}
         <br></br>
         <SingleBookItem
-          // key={key}
-          bookObj={bookMock}
-          // bookId={bookMock.bookId}
-          // authors={bookMock.authors}
-          // categories={bookMock.categories}
-          // averageRating={bookMock.averageRating}
-          // description={bookMock.description}
-          // imageLinks={bookMock.imageLinks}
-          // language={bookMock.language}
-          // pageCount={bookMock.pageCount}
-          // publisher={bookMock.publisher}
-          // title={bookMock.title}
-          userId={3}
+          bookObj={userObj?.library.currentBook}
+          userId={userObj.userId}
           location={"expanded"}
         />
       </div>
     </main>
   );
+};
+
+Library.propTypes = {
+  userObj: PropTypes.object,
 };
 
 export default Library;

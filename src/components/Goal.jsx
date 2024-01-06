689a53863c605ee984d617466f9528c75d63b0ce
MitchWAldrich <mitch.w.aldrich@gmail.com>
2024-01-05 23:49:48 -0500

    update rendering for goal add and goal update displays

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 import PropTypes from "prop-types";
 
@@ -8,34 +8,39 @@ import Dropdown from "./Dropdown";
 import Input from "./Input";
 
 import userContext from "../userContext";
-import { getGoalByGoalId } from "../utils/selectors";
+// import { getGoalByGoalId } from "../utils/selectors";
 
-import { goalsMock } from "../mocks/goals";
+// import { goalsMock } from "../mocks/goals";
 
 const Goal = (props) => {
   const user = useContext(userContext);
   let { goalObj, location } = props;
-  const { id } = useParams();
+  console.log("goalObj", goalObj);
+  // const { id } = useParams();
 
-  goalObj = getGoalByGoalId(goalsMock, id);
+  // goalObj = getGoalByGoalId(goalsMock, id);
 
   const [goalName, setGoalName] = useState(goalObj?.goalName ?? "");
   const [number, setNumber] = useState(goalObj?.goal ?? "");
   const [numberUnits, setNumberUnits] = useState(goalObj?.goalUnits ?? "");
-  const [recurrence, setRecurrence] = useState(goalObj?.goalRecurrence ?? "");
-  const [recurrenceUnits, setRecurrenceUnits] = useState(
-    goalObj?.goalRecurrenceUnits ?? ""
-  );
   const [timeline, setTimeline] = useState(goalObj?.goalTimeline ?? "");
   const [timelineUnits, setTimelineUnits] = useState(
     goalObj?.goalTimelineUnits ?? ""
   );
+  const [isRecurring, setIsRecurring] = useState(
+    goalObj?.goalRecurrence ? true : false
+  );
+  const [showIsRecurring, setShowIsRecurring] = useState(
+    isRecurring ? false : true
+  );
+  const [recurrence, setRecurrence] = useState(goalObj?.goalRecurrence ?? "");
+  const [recurrenceUnits, setRecurrenceUnits] = useState(
+    goalObj?.goalRecurrenceUnits ?? ""
+  );
+  const [showSubmit, setShowSubmit] = useState(
+    location === "update" ? (isRecurring ? false : true) : false
+  );
   const [error, setError] = useState(false);
-
-  /* {variable && (
-    some code 
-    )
-    syntax */
 
   const handleNameChange = (e) => {
     setGoalName(e.target.value);
@@ -87,6 +92,15 @@ const Goal = (props) => {
     // add book object to user object
   };
 
+  const handleIsRecurring = (value) => {
+    if (value === true) {
+      setShowIsRecurring(false);
+      setIsRecurring(true);
+    } else {
+      setShowSubmit(true);
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -132,110 +146,248 @@ const Goal = (props) => {
 
   return (
     <main className='container'>
-      <div className='GoalListItemContainer'>
-        <h3>{goalName}</h3>
-        <p>
-          I will read
-          <br />
-          {number} {numberUnits}
-          <br />
-          every
-          <br />
-          {timeline} {timelineUnits}
-          {recurrence ? (
-            <>
-              <br />
-              for
-              <br />
-              {recurrence} {recurrenceUnits}
-            </>
-          ) : null}
-        </p>
-      </div>
-      {location === "add" ? (
+      {location === "add" && (
         <>
           <h2 className='homeTitle'>Create a Reading Goal</h2>
-          <form className='form' onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor='number'>Give your Goal a name</label>
-              <div className='goalName'>
-                <Input
-                  type='text'
-                  label=''
-                  value={goalName}
-                  name='goalName'
-                  error={error}
-                  onChange={handleNameChange}
-                  placeholder='Goal Name'
-                />
-              </div>
-              <br></br>
-              <label htmlFor='number'>What is your goal?</label>
-              <div className='timeline'>
-                <Input
-                  type='number'
-                  label=''
-                  value={number}
-                  name='number'
-                  error={error}
-                  onChange={handleNumberChange}
-                  placeholder='#'
-                />
-                <br></br>
-                <Dropdown
-                  category={"Units"}
-                  options={units}
-                  dropdownName='GoalUnitsDropdown'
-                  valueCallback={getUnits}
-                />
-              </div>
-              <br></br>
-              <label htmlFor='units'>What is your goal timeline?</label>
-              <div className='timeline'>
-                <Input
-                  type='number'
-                  label=''
-                  value={timeline}
-                  name='timeline'
-                  error={error}
-                  onChange={handleTimelineChange}
-                  placeholder='#'
-                />
-                <br></br>
-                <Dropdown
-                  category={"Units"}
-                  options={timeUnits}
-                  dropdownName='GoalTimelineDropdown'
-                  valueCallback={getUnits}
-                />
-              </div>
-              <br></br>
-              <label htmlFor='units'>Is this a recurring goal?</label>
-              <div className='timeline'>
-                <Input
-                  type='number'
-                  label=''
-                  value={recurrence}
-                  name='recurrence'
-                  error={error}
-                  onChange={handleRecurrenceChange}
-                  placeholder='#'
-                />
-                <br></br>
-                <Dropdown
-                  category={"TimeUnits"}
-                  options={timeUnits}
-                  dropdownName='GoalRecurrenceDropdown'
-                  valueCallback={getUnits}
-                />
-              </div>
+          <div className='form'>
+            <label htmlFor='number'>Goal Name</label>
+            <div className='goalName'>
+              <Input
+                type='text'
+                label=''
+                value={goalName}
+                name='goalName'
+                error={error}
+                onChange={handleNameChange}
+                placeholder='Goal Name'
+              />
             </div>
-            <button className='btn' onSubmit={handleSubmit}>
-              CREATE GOAL
-            </button>
-          </form>
+            <br></br>
+            <label htmlFor='number'>What is your Goal?</label>
+            <div className='timeline'>
+              <Input
+                type='number'
+                label=''
+                value={number}
+                name='number'
+                error={error}
+                onChange={handleNumberChange}
+                placeholder='#'
+              />
+              <br></br>
+              <Dropdown
+                category={"Units"}
+                options={units}
+                defaultValue={units[2]}
+                dropdownName='GoalUnitsDropdown'
+                valueCallback={getUnits}
+              />
+            </div>
+            <br></br>
+            <label htmlFor='units'>What is your Goal Timeline?</label>
+            <div className='timeline'>
+              <Input
+                type='number'
+                label=''
+                value={timeline}
+                name='timeline'
+                error={error}
+                onChange={handleTimelineChange}
+                placeholder='#'
+              />
+              <br></br>
+              <Dropdown
+                category={"Units"}
+                options={timeUnits}
+                defaultValue={timeUnits[2]}
+                dropdownName='GoalTimelineDropdown'
+                valueCallback={getUnits}
+              />
+            </div>
+            <br></br>
+            {showIsRecurring && (
+              <>
+                <label htmlFor='units'>Is this a recurring goal?</label>
+                <button
+                  className='buttons-accept'
+                  type='button'
+                  onClick={() => handleIsRecurring(true)}
+                >
+                  YES
+                </button>
+                <button
+                  className='buttons-reject'
+                  type='button'
+                  onClick={() => handleIsRecurring(false)}
+                >
+                  NO
+                </button>
+              </>
+            )}
+            {isRecurring && (
+              <>
+                <label htmlFor='units'>How long will this goal recur?</label>
+                <div className='timeline'>
+                  <Input
+                    type='number'
+                    label=''
+                    value={recurrence}
+                    name='recurrence'
+                    error={error}
+                    onChange={handleRecurrenceChange}
+                    placeholder='#'
+                  />
+                  <br></br>
+                  <Dropdown
+                    category={"TimeUnits"}
+                    options={timeUnits}
+                    defaultValue={timeUnits[3]}
+                    dropdownName='GoalRecurrenceDropdown'
+                    valueCallback={getUnits}
+                  />
+                </div>
+                <button type='button' onSubmit={handleSubmit}>
+                  UPDATE GOAL
+                </button>
+              </>
+            )}
+            {showSubmit && (
+              <button type='button' onSubmit={handleSubmit}>
+                CREATE GOAL
+              </button>
+            )}
+          </div>
         </>
-      ) : null}
+      )}
+      {location === "update" && (
+        <>
+          <h2 className='homeTitle'>Update Your Goal</h2>
+          <div className='form'>
+            <label htmlFor='number'>Goal Name</label>
+            <div className='goalName'>
+              <Input
+                type='text'
+                label=''
+                value={goalName}
+                name='goalName'
+                error={error}
+                onChange={handleNameChange}
+                placeholder='Goal Name'
+              />
+            </div>
+            <br></br>
+            <label htmlFor='number'>Goal</label>
+            <div className='timeline'>
+              <Input
+                type='number'
+                label=''
+                value={number}
+                name='number'
+                error={error}
+                onChange={handleNumberChange}
+                placeholder='#'
+              />
+              <br></br>
+              <Dropdown
+                category={"Units"}
+                options={units}
+                defaultValue={units[2]}
+                dropdownName='GoalUnitsDropdown'
+                valueCallback={getUnits}
+              />
+            </div>
+            <br></br>
+            <label htmlFor='units'>Goal Timeline</label>
+            <div className='timeline'>
+              <Input
+                type='number'
+                label=''
+                value={timeline}
+                name='timeline'
+                error={error}
+                onChange={handleTimelineChange}
+                placeholder='#'
+              />
+              <br></br>
+              <Dropdown
+                category={"Units"}
+                options={timeUnits}
+                defaultValue={timeUnits[2]}
+                dropdownName='GoalTimelineDropdown'
+                valueCallback={getUnits}
+              />
+            </div>
+            <br></br>
+            {showIsRecurring && (
+              <>
+                <label htmlFor='units'>Add goal recurrence?</label>
+                <button
+                  className='buttons-accept'
+                  type='button'
+                  onClick={() => handleIsRecurring(true)}
+                >
+                  YES
+                </button>
+              </>
+            )}
+            {isRecurring && (
+              <>
+                <label htmlFor='units'>Goal Continues For</label>
+                <div className='timeline'>
+                  <Input
+                    type='number'
+                    label=''
+                    value={recurrence}
+                    name='recurrence'
+                    error={error}
+                    onChange={handleRecurrenceChange}
+                    placeholder='#'
+                  />
+                  <br></br>
+                  <Dropdown
+                    category={"TimeUnits"}
+                    options={timeUnits}
+                    defaultValue={timeUnits[3]}
+                    dropdownName='GoalRecurrenceDropdown'
+                    valueCallback={getUnits}
+                  />
+                </div>
+                <button type='button' onSubmit={handleSubmit}>
+                  UPDATE GOAL
+                </button>
+              </>
+            )}
+            <br></br>
+            {showSubmit && !isRecurring && (
+              <button type='button' onSubmit={handleSubmit}>
+                UPDATE GOAL
+              </button>
+            )}
+          </div>
+        </>
+      )}
+      {location === "default" && (
+        <div className='GoalListItemContainer'>
+          <h3>{goalName}</h3>
+          <p>
+            I will read
+            <br />
+            {number} {numberUnits}
+            <br />
+            every
+            <br />
+            {timeline} {timelineUnits}
+            {recurrence ? (
+              <>
+                <br />
+                for
+                <br />
+                {recurrence} {recurrenceUnits}
+              </>
+            ) : null}
+          </p>
+        </div>
+      )}
     </main>
   );
 };

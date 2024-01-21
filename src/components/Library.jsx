c3c042db30331414b124a0934b7643212c343c42
MitchWAldrich <mitch.w.aldrich@gmail.com>
2024-01-20 23:57:35 -0500

    add searchbar to update book

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,20 +1,37 @@
+import { useState } from "react";
+
 import PropTypes from "prop-types";
 
 import BookListItem from "./BookListItem";
+import SearchBar from "./Searchbar";
 import SingleBookItem from "./SingleBookItem";
 
 const Library = (props) => {
   const { userObj } = props;
+
+  const [book, setBook] = useState(userObj?.library.currentBook);
+
+  const getChosenSearchResults = (searchValue) => {
+    setBook(searchValue);
+  };
 
   return (
     <main>
       <div>
         <h2>What I'm Reading</h2>
         <SingleBookItem
-          bookObj={userObj?.library.currentBook}
+          bookObj={book}
           userId={userObj.userId}
           location={"expanded"}
           isBookInLibrary={true}
+        />
+        <h2>Change Book?</h2>
+        <SearchBar
+          className='searchBar'
+          location='home'
+          dropDown={true}
+          id={userObj.userId}
+          valueCallback={getChosenSearchResults}
         />
         <h2>To Read List</h2>
         {userObj?.library?.toRead.map((book, key) => (
